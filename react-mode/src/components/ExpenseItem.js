ecacdd5f9d5b940ddd2d05f81e179ac8ca3d4742
icenine79 <vitor.m.c.n.ferreira@gmail.com>
2021-10-27 13:06:40 +0100

    a

--- react-mode/src/components/ExpenseItem.js
+++ react-mode/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-
+const expenseDate = new Date(2021,2,28);
+const expenseTitle = "Car Insurance";
+const expenseAmount = 300;
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expenseDate.toISOString()}</div>
             <h2 className="expense-item__description">Car Insurance</h2>
-            <div className="expense-item__price">€300</div>
+            <h2>{expenseAmount}</h2>
+            <div className="expense-item__price">€{expenseAmount}</div>
         </div>);
 
 
